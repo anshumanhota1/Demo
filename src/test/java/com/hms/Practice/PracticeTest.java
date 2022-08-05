ce9121fc3add9908355c6b244c8657fcf6f31cac
Dipeekas1234 <dipeekasabbanwar@gmail.com>
2022-08-05 10:56:11 +0530

    commit done

--- src/test/java/com/hms/Practice/PracticeTest.java
+++ src/test/java/com/hms/Practice/PracticeTest.java
@@ -6,5 +6,7 @@ public void Printings()
 {
 	System.out.println("trial and error checking for push and pull request");
 	System.out.println("heeyy");
+	System.out.println("changes done ------");
+	
 }
 }
